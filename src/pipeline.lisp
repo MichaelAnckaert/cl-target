3d7b8df41630ddbb8d45052abaca7064c56648f2
Michael Anckaert <michael.anckaert@sinax.be>
2023-08-17 18:07:43 +0200

    Implement office target calculation

--- src/pipeline.lisp
+++ src/pipeline.lisp
@@ -39,30 +39,55 @@
           :collect current
           :do (setq current (local-time:timestamp+ current step unit)))))
 
-(defun calculate-daily-targets (target email date-range)
+(defun calculate-daily-targets (target identifier date-range &optional unit)
   (let* ((day-count (length date-range))
          (daily-target (/ target day-count)))
-    (loop for date in date-range
-          :collect (list
-                    (local-time:format-timestring nil date :format '(:day "/" :month "/" :year))
-                    email
-                    daily-target))))
+    (if (eq unit nil)
+        (loop for date in date-range
+              :collect (list
+                        (local-time:format-timestring nil date :format '(:day "/" :month "/" :year))
+                        identifier
+                        daily-target))
+        (loop for date in date-range
+              :collect (list
+                        (local-time:format-timestring nil date :format '(:day "/" :month "/" :year))
+                        identifier
+                        unit
+                        daily-target)))))
 
-(defun process-record (record)
+(defun process-collaborator-record (record)
   (let* ((year (parse-int (first record)))
          (email (second record))
          (target (parse-float (third record)))
          (range (timestamp-range year)))
     (calculate-daily-targets target email range)))
 
-(defun prepend-header (data)
-  (cons '("data" "collaborator" "target") data))
+(defun process-office-record (record)
+  (let* ((year (parse-int (first record)))
+         (office (second record))
+         (unit (third record))
+         (target (parse-float (fourth record)))
+         (range (timestamp-range year)))
+    (calculate-daily-targets target office range unit)))
 
-(defun run-pipeline (inputfile outputfile)
+(defun prepend-collaborator-header (data)
+  (cons '("date" "collaborator" "target") data))
+
+(defun prepend-office-header (data)
+  (cons '("date" "office" "unit" "target") data))
+
+(defun run-collaborator-pipeline (inputfile outputfile)
   (arrows:->>
    (read-input (truename inputfile))
-   (mapcan #'process-record)
-   (prepend-header)
+   (mapcan #'process-collaborator-record)
+   (prepend-collaborator-header)
+   (write-output outputfile)))
+
+(defun run-office-pipeline (inputfile outputfile)
+  (arrows:->>
+   (read-input (truename inputfile))
+   (mapcan #'process-office-record)
+   (prepend-office-header)
    (write-output outputfile)))
 
 (defun display-help ()
@@ -73,22 +98,29 @@
 
 (defun main ()
   (opts:define-opts
-      (:name :help
-             :description "print this help text"
-             :short #\h
-             :long "help")
-      (:name :inputfile
-             :description "the input file to process"
-             :short #\i
-             :long "input"
-             :arg-parser #'identity
-             :meta-var "FILE")
+    (:name :help
+     :description "print this help text"
+     :short #\h
+     :long "help")
+    (:name :type
+     :description "the type of target to process"
+     :short #\t
+     :long "type"
+     :arg-parser #'identity
+     :default "collaborator"
+     :meta-var "TYPE")
+    (:name :inputfile
+     :description "the input file to process"
+     :short #\i
+     :long "input"
+     :arg-parser #'identity
+     :meta-var "FILE")
     (:name :outputfile
-           :description "the file to write the output to"
-           :short #\o
-           :long "output"
-           :arg-parser #'identity
-           :meta-var "FILE"))
+     :description "the file to write the output to"
+     :short #\o
+     :long "output"
+     :arg-parser #'identity
+     :meta-var "FILE"))
 
   (multiple-value-bind (options free-args)
       (opts:get-opts)
@@ -101,4 +133,6 @@
     
     (let ((inputfile (getf options :inputfile))
           (outputfile (getf options :outputfile)))
-      (run-pipeline inputfile outputfile))))
+      (if (string= (getf options :type) "office")
+          (run-office-pipeline inputfile outputfile)          
+          (run-collaborator-pipeline inputfile outputfile)))))
